105c91044ba5b7b9484b0bf467b84e48ccc747e3
MaikeruFurora <34005796+MaikeruFurora@users.noreply.github.com>
2024-04-12 11:52:59 +0800

    CHANGES COSTING
    CHANGES COSTING

--- arvin-portal/src/App.js
+++ arvin-portal/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import IndexHome from "./apps/aim/home/pages/IndexHome";
 import Navigation from "./apps/navigation/pages/Navigation";
 import Loader from "./components/loading/Loading";
+import CostingItemList from "./apps/aim/costing/itemList/pages/indexItemList";
 const theme = createTheme({
   typography: {
     fontFamily: "Lexend Deca, Helvetica, Arial, sans-serif",
@@ -20,6 +21,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Navigation />}>
               <Route path="/" element={<IndexHome />} />
+              <Route path="/costing/itemlist" element={<CostingItemList />} />
             </Route>
           </Routes>
         </Suspense>
---
+++ arvin-portal/src/apps/aim/costing/itemList/pages/indexItemList.jsx
@@ -0,0 +1,63 @@
+import { Grid } from "@mui/material";
+import * as React from "react";
+import configure from "../../../../configure/configure";
+import BreadCrumbs from '../../../../../components/breadCrumb/BreadCrumbs';
+import HomeIcon from "@mui/icons-material/Home";
+import CalculateIcon from '@mui/icons-material/Calculate';
+import TocIcon from '@mui/icons-material/Toc';
+
+const breadCrumbArray = [
+  {
+    name: "Home",
+    href: "",
+    icon: (
+      <HomeIcon
+        sx={{
+          mr: 0.5,
+          fontSize: configure.bread_crumbs_font_size,
+          color: configure.primary_color,
+        }}
+      />
+    ),
+  },
+  {
+    name: "Costing",
+    href: "",
+    icon: (
+      <CalculateIcon
+        sx={{
+          mr: 0.5,
+          fontSize: configure.bread_crumbs_font_size,
+          color: configure.primary_color,
+        }}
+      />
+    ),
+  },
+  {
+    name: "Item List",
+    href: "",
+    icon: (
+      <TocIcon
+        sx={{
+          mr: 0.5,
+          fontSize: configure.bread_crumbs_font_size,
+          color: configure.primary_color,
+        }}
+      />
+    ),
+  },
+];
+export default function CostingItemList() {
+  return (
+    <React.Fragment>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <BreadCrumbs breadCrumbs={breadCrumbArray} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
